a7292430d59558cc3caa7819142c5c61f13a75f5
Louan Ourvouai <megakitraz3000@gmail.com>
2025-02-07 12:05:56 +0100

    Switch advanced, to fix

--- Assets/Scripts/Gameplay/Server/ServerGameSystem.cs
+++ Assets/Scripts/Gameplay/Server/ServerGameSystem.cs
@@ -23,26 +23,15 @@ namespace Unity.Template.CompetitiveActionMultiplayer
         public float Delay;
     }
 
-    /// <summary>
-    /// This buffer is used to map clients using their <see cref="NetworkId"/> index as a key and this struct as a value,
-    /// making it easy to find Entities relating to that specific client.
-    /// </summary>
     public struct GameplayMaps : IBufferElementData
     {
-        /// <summary>The <see cref="NetworkStreamConnection"/> entity for this <see cref="NetworkId"/> index.</summary>
         public Entity ConnectionEntity;
-        /// <summary>The <see cref="FirstPersonPlayer"/> entity for this <see cref="NetworkId"/> index.</summary>
         public Entity FirstPersonPlayersEntity;
-        /// <summary>The <see cref="FirstPersonCharacterControl"/> entity for this <see cref="NetworkId"/> index.</summary>
         public Entity CharacterControllerEntity;
-
-        /// <summary>If != default, need to remap this to the entity.</summary>
         public NetworkId RemapTo;
     }
 
-    /// <summary>
-    /// Processes client join requests and spawns a character for each one of them.
-    /// </summary>
+    [UpdateInGroup(typeof(SimulationSystemGroup), OrderFirst = true)] // Use SimulationSystemGroup as a fallback
     [WorldSystemFilter(WorldSystemFilterFlags.ServerSimulation)]
     [BurstCompile]
     public partial struct ServerGameSystem : ISystem
@@ -58,7 +47,6 @@ namespace Unity.Template.CompetitiveActionMultiplayer
             state.RequireForUpdate<NetworkStreamDriver>();
             state.RequireForUpdate<GameplayMaps>();
 
-            // Creates random singleton
             var randomSeed = (uint)DateTime.Now.Millisecond;
             Entity randomEntity = state.EntityManager.CreateEntity();
             state.EntityManager.AddComponentData(randomEntity, new FixedRandom
@@ -67,7 +55,7 @@ namespace Unity.Template.CompetitiveActionMultiplayer
             });
 
             var mapSingleton = state.EntityManager.CreateSingletonBuffer<GameplayMaps>();
-            state.EntityManager.GetBuffer<GameplayMaps>(mapSingleton).Add(default); // Default entry for index 0 (the server NetworkId index).
+            state.EntityManager.GetBuffer<GameplayMaps>(mapSingleton).Add(default);
         }
 
         [BurstCompile]
@@ -109,9 +97,6 @@ namespace Unity.Template.CompetitiveActionMultiplayer
                     gameplayMaps.ElementAt(evt.Id.Value) = default;
             }
 
-            // Entities created via ECB have temporary Entity IDs.
-            // These are not updated correctly - for Dynamic Buffers - unless we use ECB.AppendToBuffer
-            // but this is an index lookup. So patch them.
             for (var i = gameplayMaps.Length - 1; i >= 0; i--)
             {
                 ref var map = ref gameplayMaps.ElementAt(i);
@@ -131,7 +116,6 @@ namespace Unity.Template.CompetitiveActionMultiplayer
 
         void HandleJoinRequests(ref SystemState state, Entity gameplayMapsEntity, GameResources gameResources, EntityCommandBuffer ecb)
         {
-            // Process join requests
             foreach (var (request, rpcReceive, entity) in
                      SystemAPI.Query<ClientJoinRequestRpc, ReceiveRpcCommandRequest>().WithEntityAccess())
             {
@@ -140,37 +124,33 @@ namespace Unity.Template.CompetitiveActionMultiplayer
                 {
                     var ownerNetworkId = SystemAPI.GetComponent<NetworkId>(rpcReceive.SourceConnection);
 
-                    // Spawn player
                     var playerEntity = ecb.Instantiate(gameResources.PlayerGhost);
                     ecb.AppendToBuffer(gameplayMapsEntity, new GameplayMaps
                     {
                         RemapTo = ownerNetworkId,
                         FirstPersonPlayersEntity = playerEntity,
                     });
-                    ecb.SetComponent(playerEntity, new GhostOwner { NetworkId = ownerNetworkId.Value });
+
+                    ecb.AddComponent(playerEntity, new GhostOwner { NetworkId = ownerNetworkId.Value });
                     ecb.AppendToBuffer(rpcReceive.SourceConnection, new LinkedEntityGroup { Value = playerEntity });
 
-                    // Set player data
                     FirstPersonPlayer player = SystemAPI.GetComponent<FirstPersonPlayer>(gameResources.PlayerGhost);
                     player.Name = request.PlayerName;
                     ecb.SetComponent(playerEntity, player);
 
                     if (!request.IsSpectator)
                     {
-                        // Request to spawn character
                         Entity spawnCharacterRequestEntity = ecb.CreateEntity();
                         ecb.AddComponent(spawnCharacterRequestEntity,
                             new SpawnCharacter { ClientEntity = rpcReceive.SourceConnection, Delay = -1f });
                     }
 
-                    // Remember player for connection
                     ecb.AddComponent(rpcReceive.SourceConnection, new JoinedClient { PlayerEntity = playerEntity });
-                    // Stream in game
                     ecb.AddComponent(rpcReceive.SourceConnection, new NetworkStreamInGame());
 
                     state.EntityManager.GetName(gameResources.PlayerGhost, out var playerNameFs);
                     if (playerNameFs.IsEmpty) playerNameFs = nameof(gameResources.PlayerGhost);
-                    Debug.Log($"[{state.WorldUnmanaged.Name}] Spawning '{playerNameFs}' (the netcode input wrapper) for {ownerNetworkId.ToFixedString()} called '{request.PlayerName}'!");
+                    Debug.Log($"[{state.WorldUnmanaged.Name}] Spawning '{playerNameFs}' for {ownerNetworkId.ToFixedString()} called '{request.PlayerName}'!");
                 }
                 ecb.DestroyEntity(entity);
             }
@@ -178,7 +158,6 @@ namespace Unity.Template.CompetitiveActionMultiplayer
 
         void HandleCharacters(ref SystemState state, GameResources gameResources, Entity gameplayMapsEntity, EntityCommandBuffer ecb)
         {
-            // Initialize characters common
             foreach (var (physicsCollider, characterInitialized, entity) in SystemAPI
                          .Query<RefRW<PhysicsCollider>, EnabledRefRW<CharacterInitialized>>()
                          .WithAll<FirstPersonCharacterComponent>()
@@ -186,12 +165,9 @@ namespace Unity.Template.CompetitiveActionMultiplayer
                          .WithEntityAccess())
             {
                 physicsCollider.ValueRW.MakeUnique(entity, ecb);
-
-                // Mark initialized
                 characterInitialized.ValueRW = true;
             }
 
-            // Spawn character requests
             if (SystemAPI.QueryBuilder().WithAll<SpawnCharacter>().Build().CalculateEntityCount() > 0)
             {
                 var spawnPointsQuery = SystemAPI.QueryBuilder().WithAll<SpawnPoint, LocalToWorld>().Build();
@@ -211,39 +187,32 @@ namespace Unity.Template.CompetitiveActionMultiplayer
                         if (SystemAPI.HasComponent<NetworkId>(spawnRequest.ValueRW.ClientEntity) &&
                             SystemAPI.HasComponent<JoinedClient>(spawnRequest.ValueRW.ClientEntity))
                         {
-                            // Try to find a free (i.e. unblocked by other players) spawn point:
-                            if (!TryFindSpawnPoint(gameResources, spawnPointLtWs, random, consumedSpawnPoints,
-                                    out var spawnPoint))
+                            if (!TryFindSpawnPoint(gameResources, spawnPointLtWs, random, consumedSpawnPoints, out var spawnPoint))
                                 break;
 
                             var ownerNetworkId = SystemAPI.GetComponent<NetworkId>(spawnRequest.ValueRW.ClientEntity);
-                            Entity playerEntity = SystemAPI.GetComponent<JoinedClient>(spawnRequest.ValueRW.ClientEntity)
-                                .PlayerEntity;
+                            Entity playerEntity = SystemAPI.GetComponent<JoinedClient>(spawnRequest.ValueRW.ClientEntity).PlayerEntity;
 
-                            // Set character Y rotation before spawn
                             var character = SystemAPI.GetComponent<FirstPersonCharacterComponent>(gameResources.CharacterGhost);
                             character.CharacterYDegrees = FirstPersonCharacterUtilities.ComputeCharacterYAngleFromDirection(spawnPoint.Forward);
                             ecb.SetComponent(gameResources.CharacterGhost, character);
 
-                            // Spawn character
                             var characterEntity = ecb.Instantiate(gameResources.CharacterGhost);
                             ecb.AppendToBuffer(gameplayMapsEntity, new GameplayMaps
                             {
                                 RemapTo = ownerNetworkId,
                                 CharacterControllerEntity = characterEntity,
                             });
-                            ecb.SetComponent(characterEntity, new GhostOwner { NetworkId = ownerNetworkId.Value });
+                            ecb.AddComponent(characterEntity, new GhostOwner { NetworkId = ownerNetworkId.Value });
                             ecb.SetComponent(characterEntity, LocalTransform.FromPositionRotation(spawnPoint.Position, spawnPoint.Rotation));
                             ecb.SetComponent(characterEntity, new OwningPlayer { Entity = playerEntity });
 
                             ecb.AppendToBuffer(spawnRequest.ValueRW.ClientEntity, new LinkedEntityGroup { Value = characterEntity });
 
-                            // Assign character to player
                             FirstPersonPlayer player = SystemAPI.GetComponent<FirstPersonPlayer>(playerEntity);
                             player.ControlledCharacter = characterEntity;
                             ecb.SetComponent(playerEntity, player);
 
-                            // Spawn & assign starting weapon
                             Entity randomWeaponPrefab;
                             int weaponIndex;
                             if (gameResources.ForceOnlyFirstWeapon)
@@ -257,15 +226,14 @@ namespace Unity.Template.CompetitiveActionMultiplayer
                                 randomWeaponPrefab = weaponPrefabs[weaponIndex].WeaponPrefab;
                             }
 
-                            // Weapon
                             Entity weaponEntity = ecb.Instantiate(randomWeaponPrefab);
-                            ecb.SetComponent(weaponEntity, new GhostOwner { NetworkId = ownerNetworkId.Value });
-                            ecb.SetComponent(weaponEntity, new WeaponPrefabIndex { Index = weaponIndex }); // Track weapon index
-                            ecb.SetComponent(characterEntity, new ActiveWeapon { Entity = weaponEntity });
+                            ecb.AddComponent(weaponEntity, new GhostOwner { NetworkId = ownerNetworkId.Value });
+                            ecb.SetComponent(weaponEntity, new WeaponPrefabIndex { Index = weaponIndex });
+                            ecb.SetComponent(playerEntity, new ActiveWeapon { Entity = weaponEntity });
 
                             state.EntityManager.GetName(gameResources.CharacterGhost, out var characterNameFs);
                             if (characterNameFs.IsEmpty) characterNameFs = nameof(gameResources.CharacterGhost);
-                            Debug.Log($"[{state.WorldUnmanaged.Name}] Spawning (or respawning) character controller '{characterNameFs}' for {ownerNetworkId.ToFixedString()}!");
+                            Debug.Log($"[{state.WorldUnmanaged.Name}] Spawning character '{characterNameFs}' for {ownerNetworkId.ToFixedString()}!");
                         }
 
                         ecb.DestroyEntity(entity);
@@ -290,7 +258,6 @@ namespace Unity.Template.CompetitiveActionMultiplayer
                     var spawnPointIndex = (randSpawnPointIndex + attempt) % spawnPointLtWs.Length;
                     if (!consumedSpawnPoints.IsSet(spawnPointIndex))
                     {
-                        Debug.Assert(gameResources.SpawnPointCollisionFilter.CollidesWith != default);
                         var spawnPointBlocked = collisionWorld.CheckSphere(
                             spawnPointLtWs[spawnPointIndex].Position,
                             gameResources.SpawnPointBlockRadius,
@@ -321,9 +288,9 @@ namespace Unity.Template.CompetitiveActionMultiplayer
                     var health = state.EntityManager.GetComponentData<Health>(characterControllerEntity);
                     health.CurrentHealth = 0;
                     ecb.SetComponent(characterControllerEntity, health);
-                    Debug.Log($"[{state.WorldUnmanaged.Name}] Client {ownerNetworkId.ToFixedString()} requested respawn, killing their player {characterControllerEntity.ToFixedString()}!");
+                    Debug.Log($"[{state.WorldUnmanaged.Name}] Client {ownerNetworkId.ToFixedString()} requested respawn!");
                 }
-                else Debug.LogWarning($"[{state.WorldUnmanaged.Name}] Client {ownerNetworkId.ToFixedString()} requested respawn, but CC {characterControllerEntity.ToFixedString()} has no Health. Probably deleted.");
+                else Debug.LogWarning($"[{state.WorldUnmanaged.Name}] Respawn failed for {ownerNetworkId.ToFixedString()}");
                 ecb.DestroyEntity(rpcEntity);
             }
         }
--- Assets/Scripts/Gameplay/Weapons/WeaponSwitchSystem.cs
+++ Assets/Scripts/Gameplay/Weapons/WeaponSwitchSystem.cs
@@ -4,6 +4,7 @@ using Unity.Entities;
 using Unity.NetCode;
 using Unity.Mathematics;
 using static Unity.Template.CompetitiveActionMultiplayer.ServerGameSystem;
+using UnityEngine;
 
 namespace Unity.Template.CompetitiveActionMultiplayer
 {
@@ -13,7 +14,6 @@ namespace Unity.Template.CompetitiveActionMultiplayer
     [BurstCompile]
     public partial struct WeaponSwitchSystem : ISystem
     {
-        [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
             var ecb = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>()
@@ -24,27 +24,59 @@ namespace Unity.Template.CompetitiveActionMultiplayer
                 .WithEntityAccess())
             {
                 Entity connectionEntity = receiveRpc.SourceConnection;
+                if (!SystemAPI.HasComponent<JoinedClient>(connectionEntity))
+                {
+                    ecb.DestroyEntity(entity);
+                    continue;
+                }
 
-                if (!SystemAPI.HasComponent<JoinedClient>(connectionEntity)) continue;
                 Entity playerEntity = SystemAPI.GetComponent<JoinedClient>(connectionEntity).PlayerEntity;
 
-                if (!SystemAPI.HasComponent<ActiveWeapon>(playerEntity)) continue;
+                // Ensure the player has a GhostOwner component (critical for NetCode ownership)
+                if (!SystemAPI.HasComponent<GhostOwner>(playerEntity))
+                {
+                    Debug.LogError("Player entity is missing GhostOwner component!");
+                    ecb.DestroyEntity(entity);
+                    continue;
+                }
+                var playerGhostOwner = SystemAPI.GetComponent<GhostOwner>(playerEntity);
+
+                // Initialize ActiveWeapon if missing
+                if (!SystemAPI.HasComponent<ActiveWeapon>(playerEntity))
+                {
+                    var gameResources = SystemAPI.GetSingleton<GameResources>();
+                    var tmp_weapons = SystemAPI.GetSingletonBuffer<GameResourcesWeapon>();
+                    Entity defaultWeaponPrefab = tmp_weapons[0].WeaponPrefab;
+                    Entity tmp_newWeapon = ecb.Instantiate(defaultWeaponPrefab);
+
+                    // Assign ownership to the player
+                    ecb.AddComponent(tmp_newWeapon, new GhostOwner { NetworkId = playerGhostOwner.NetworkId });
+
+                    ecb.AddComponent(playerEntity, new ActiveWeapon { Entity = tmp_newWeapon });
+                    Debug.LogWarning("Initialized default weapon for player.");
+                    ecb.DestroyEntity(entity); // Destroy RPC after handling
+                    continue; // Skip further processing this frame
+                }
+
                 ActiveWeapon activeWeapon = SystemAPI.GetComponent<ActiveWeapon>(playerEntity);
+                if (!state.EntityManager.Exists(activeWeapon.Entity))
+                {
+                    Debug.LogError("Active weapon entity is invalid!");
+                    ecb.DestroyEntity(entity);
+                    continue;
+                }
 
-                if (!state.EntityManager.Exists(activeWeapon.Entity)) continue;
-
-                // Get index from prefab component
-                if (!SystemAPI.HasComponent<WeaponPrefabIndex>(activeWeapon.Entity)) continue;
+                // Calculate new weapon index
                 int currentIndex = SystemAPI.GetComponent<WeaponPrefabIndex>(activeWeapon.Entity).Index;
-
-                var gameResources = SystemAPI.GetSingleton<GameResources>();
                 var weapons = SystemAPI.GetSingletonBuffer<GameResourcesWeapon>();
                 int newIndex = (currentIndex + rpc.ValueRO.Direction + weapons.Length) % weapons.Length;
 
-                // Spawn new weapon (index comes from prefab)
+                // Spawn new weapon and assign ownership
                 Entity newWeaponPrefab = weapons[newIndex].WeaponPrefab;
                 Entity newWeapon = ecb.Instantiate(newWeaponPrefab);
+                ecb.AddComponent(newWeapon, new GhostOwner { NetworkId = playerGhostOwner.NetworkId });
 
+                // Update player's ActiveWeapon and clean up
                 ecb.SetComponent(playerEntity, new ActiveWeapon { Entity = newWeapon });
                 ecb.DestroyEntity(activeWeapon.Entity);
                 ecb.DestroyEntity(entity);
